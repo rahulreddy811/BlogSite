b7d90624524101154138e7b88b473406d34d9c59
rahulreddy811 <srahulreddy424@gmail.com>
2026-06-08 00:07:37 +0530

    changes to upload image

--- utils/resizer.py
+++ utils/resizer.py
@@ -1,17 +1,20 @@
-from PIL import Image,ImageDraw
+from PIL import Image, ImageDraw
 import os
 
-def save_and_resize(image_file,UPLOAD_FOLDER,filename):
-    path = os.path.join(UPLOAD_FOLDER,filename)
-    img = Image.open(image_file)
-    img = img.convert("RGB")
-    img.thumbnail((200,200))
-    mask = Image.new('L', (200, 200), 0)
-    draw = ImageDraw.Draw(mask)
-    draw.ellipse((0, 0, 200, 200), fill=255)
+def save_and_resize(image_file, upload_folder, filename):
+    path = os.path.join(upload_folder, filename)
 
-    # Apply mask
+    img = Image.open(image_file).convert("RGBA")
+
+    img = img.resize((200, 200))
+
+    # create circular mask same size as image
+    mask = Image.new("L", img.size, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0, img.size[0], img.size[1]), fill=255)
+
     img.putalpha(mask)
 
-    img.save(path)
+    img.save(path, format="PNG")  # IMPORTANT
+
     return path
